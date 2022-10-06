297f3212be3c607469fdd0e01bbf67059c800c17
Sebastian Rakel <sebastian@devunit.eu>
2022-10-06 11:52:09 +0200

    Initial version

---
+++ manifests/init.pp
@@ -0,0 +1,22 @@
+# @summary Installs a firebird server
+class firebird(
+  String[1] $service_name,
+  String[1] $config_path,
+  String[1] $user,
+  String[1] $group,
+  String[1] $package_name,
+  String[1] $version,
+  Hash $config,
+  Boolean $manage_service = true,
+  Boolean $manage_package = true,
+){
+  if $manage_package {
+    include firebird::install
+  }
+
+  include firebird::config
+
+  if $manage_service {
+    include firebird::service
+  }
+}
---
+++ manifests/install.pp
@@ -0,0 +1,19 @@
+# @summary Installs firebird server
+class firebird::install(
+  String $package_name = $firebird::package_name,
+  String $version = $firebird::version,
+){
+  case $facts['os']['name'] {
+    'Windows':  {
+      package { $package_name:
+        ensure   => $version,
+        provider => 'chocolatey',
+      }
+    }
+    default: {
+      package { $package_name:
+        ensure => $version,
+      }
+    }
+  }
+}
